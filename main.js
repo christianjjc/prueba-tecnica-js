ec42ab0a102585220b78c4f17bd348655c3e68a2
christianjjc <christianjjc@gmail.com>
2024-02-22 22:48:18 -0500

    flecha del boton ok

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 let toggleVisible = false;
 const opciones = ['activo', 'inactivo','borrador', 'publicado'];
 const boton = document.getElementById("btn");
+const seleccionado = document.getElementById("texto-opcion");
 const contenedor = document.getElementById("contenedor");
+
+const arrowin = document.getElementById("arrowin")
+const arrowout= document.getElementById("arrowout")
 
 const cargarOpciones=(array)=>{
     let i = 1;
     for (const el of array) {
         const divOpciones = document.createElement("div")
         divOpciones.setAttribute("id",`opt-${i}`);
-        divOpciones.innerText=el
+        divOpciones.innerHTML=el
         divOpciones.classList.add("option");
         contenedor.appendChild(divOpciones)
         i++
@@ -20,17 +24,23 @@ const cargarOpciones=(array)=>{
 
 boton.addEventListener("click",()=>{
     if (!toggleVisible) {
-        contenedor.classList.remove("oculto");
-        toggleVisible = true;
+      contenedor.classList.remove("oculto");
+      arrowin.classList.add("oculto");
+      arrowout.classList.remove("oculto");
+      toggleVisible = true;
     } else {
-        contenedor.classList.add("oculto");
-        toggleVisible = false;
+      contenedor.classList.add("oculto");
+      arrowin.classList.remove("oculto");
+      arrowout.classList.add("oculto");
+      toggleVisible = false;
     }
 })
 
 function seleccionar(divOpciones){
-    boton.textContent = divOpciones.textContent
+    seleccionado.innerHTML  = divOpciones.innerHTML
     contenedor.classList.add("oculto");
+    arrowout.classList.add("oculto");
+    arrowin.classList.remove("oculto");
     toggleVisible = false;
 }
 
